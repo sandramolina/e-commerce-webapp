e896eba95281a4528dbc2836fc925e778fd1801e
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-02 22:22:46 +0100

    comments app test

---
+++ client/src/tbd.js
@@ -0,0 +1,18 @@
+/* eslint-disable jest/no-commented-out-tests */
+// import React from 'react';
+
+// import { render } from '@testing-library/react';
+// import { Provider } from 'react-redux';
+
+// import { store } from './app/store';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   );
+
+//   expect(getByText(/learn/i)).toBeInTheDocument();
+// });
