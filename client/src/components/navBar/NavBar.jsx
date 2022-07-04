132aa23442553e3df40903afb99532be06b707ac
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-04 14:14:43 +0100

    creates img folder in src

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -12,6 +12,10 @@ import {
 
 import '../../css/main.min.css';
 
+import shoppingBag from '../../images/icons/shoppingbag.svg';
+// eslint-disable-next-line spaced-comment
+//import searchIcon from '../../images/icons/search.svg';
+
 function NavBar() {
   return (
     <Navbar bg='light' expand='lg' fixed='top'>
@@ -25,6 +29,13 @@ function NavBar() {
             alt='So fetch! makeup logo'
           />
         </Navbar.Brand>
+        <Nav>
+          <img
+            src={shoppingBag}
+            alt='Shopping cart'
+            className='shopping-bag d-flex'
+          />
+        </Nav>
         <Navbar.Toggle aria-controls='navbarScroll' />
         <Navbar.Collapse id='navbarScroll'>
           <Nav
@@ -33,7 +44,6 @@ function NavBar() {
             navbarScroll
           >
             <Nav.Link href='#action1'>Home</Nav.Link>
-            <Nav.Link href='#action2'>Link</Nav.Link>
             <NavDropdown title='Link' id='navbarScrollingDropdown'>
               <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
               <NavDropdown.Item href='#action4'>
@@ -44,9 +54,6 @@ function NavBar() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href='#' disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className='d-flex'>
             <FormControl
