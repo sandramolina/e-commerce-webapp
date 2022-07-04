c6117b42e49f0dc58010b3c9539bdebf91cfa185
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-04 12:01:16 +0100

    adds navbar compnent

---
+++ client/src/components/navBar/NavBar.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import {
+  Button,
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  NavDropdown,
+} from 'react-bootstrap';
+
+import '../../css/main.min.css';
+
+function NavBar() {
+  return (
+    <Navbar bg='light' expand='lg' fixed='top'>
+      <Container fluid>
+        <Navbar.Brand href='#home'>
+          <img
+            src='/logo.png'
+            width='100'
+            height='50'
+            className='d-inline-block align-top'
+            alt='So fetch! makeup logo'
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className='me-auto my-2 my-lg-0'
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href='#action1'>Home</Nav.Link>
+            <Nav.Link href='#action2'>Link</Nav.Link>
+            <NavDropdown title='Link' id='navbarScrollingDropdown'>
+              <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action4'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action5'>
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href='#' disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className='d-flex'>
+            <FormControl
+              type='search'
+              placeholder='Search'
+              className='me-2'
+              aria-label='Search'
+            />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
