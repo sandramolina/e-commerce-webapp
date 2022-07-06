0362a3a5d472b4ed21acc0240455edb7e4486631
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-06 14:45:04 +0100

    calculates the cart total

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -14,10 +14,19 @@ function Cart() {
   const productsInCartNodes = productsInCart.map((productInCart) => (
     <CartItem productInCart={productInCart} key={productInCart.id} />
   ));
+
+  const totalCart = productsInCart.reduce(
+    (runninTotal, currentCartItem) =>
+      runninTotal +
+      currentCartItem.product.price.amount * currentCartItem.count,
+    0
+  );
   return (
     <ul>
       <h4>{productsInCart.length} items in the bag:</h4>
       <div>{productsInCartNodes} </div>
+      <hr />
+      <h3>Total purchase: £{totalCart}</h3>
       {productsInCart.length !== 0 ? (
         <Button onClick={handleClick}>Checkout</Button>
       ) : null}
