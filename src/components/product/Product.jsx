e9feca1af4ea37b026ca9ce816b02a52693e6c2e
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-10 10:52:36 +0100

    removes add to cart funct from product card

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import { addItemToCart } from '../cart/CartItemSlice';
 import '../../css/main.min.css';
 import './Product.css';
 import { ReactComponent as Heart } from '../../images/icons/heart-fill.svg';
@@ -12,24 +11,7 @@ import { ReactComponent as Heart } from '../../images/icons/heart-fill.svg';
 import { updateFavourites } from './ProductSlice';
 
 const Product = ({ product }) => {
-  // Setting itemCart object that will be added to the cart:
-  const [count, setCount] = useState(0);
-
-  const handleCountSelection = (event) => setCount(event.target.value);
-
   const dispatch = useDispatch();
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const itemCount = Number(count);
-
-    const id = Math.floor(Math.random() * 100 + 1);
-    const itemCartObject = {
-      id,
-      product,
-      count: itemCount,
-    };
-    dispatch(addItemToCart(itemCartObject));
-  };
 
   // Checking if certaing product is included in the favourite list:
   const favesArray = useSelector(({ productsState }) => productsState.faves);
@@ -54,17 +36,6 @@ const Product = ({ product }) => {
           <Card.Text>
             {product.price.currencyUnit.symbol} {product.price.amount}
           </Card.Text>
-          <form onSubmit={handleFormSubmit}>
-            <select name='count' onChange={handleCountSelection}>
-              <option value='0' defaultValue>
-                -Chose quantity-
-              </option>
-              <option value='1'>1</option>
-              <option value='2'>2</option>
-              <option value='3'>3</option>
-            </select>
-            <input type='submit' value='addToCart' />
-          </form>
           <Heart
             className={isFave ? 'heart-faved' : 'heart'}
             onClick={() => dispatch(updateFavourites(product))}
