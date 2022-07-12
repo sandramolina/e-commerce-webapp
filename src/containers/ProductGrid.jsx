01a0c696be0a1050fb91341a4b6c8d833851b898
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-12 20:24:26 +0100

    adds margin to top and left

--- src/containers/ProductGrid.jsx
+++ src/containers/ProductGrid.jsx
@@ -23,14 +23,14 @@ const ProductGrid = () => {
     <Product product={product} key={product.id} />
   ));
   return (
-    <>
+    <div className='product-grid'>
       <h1 className='product-title'> {categoryTitle} Products</h1>
       {productNodes.length !== 0 ? (
         <div className='products'>{productNodes} </div>
       ) : (
         <h3>No products to show</h3>
       )}
-    </>
+    </div>
   );
 };
 
