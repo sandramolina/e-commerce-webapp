69c0ea2cb73690e8e6ab5cff5fc258616a056317
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-12 13:06:17 +0100

    adds checkout form

--- src/components/cart/CheckOutForm.jsx
+++ src/components/cart/CheckOutForm.jsx
@@ -3,26 +3,30 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const CheckOutForm = () => (
-  <Form>
-    <Form.Group className='mb-3' controlId='formBasicEmail'>
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type='email' placeholder='Enter email' />
-      <Form.Text className='text-muted'>
-        Well never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+const CheckOutForm = () => {
+  const handleSubmitClick = (event) => event.preventDefault();
 
-    <Form.Group className='mb-3' controlId='formBasicPassword'>
-      <Form.Label>Password</Form.Label>
-      <Form.Control type='password' placeholder='Password' />
-    </Form.Group>
-    <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-      <Form.Check type='checkbox' label='Check me out' />
-    </Form.Group>
-    <Button variant='primary' type='submit'>
-      Submit
-    </Button>
-  </Form>
-);
+  return (
+    <Form>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type='email' placeholder='Enter email' />
+        <Form.Text className='text-muted'>
+          Well never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className='mb-3' controlId='formBasicPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type='password' placeholder='Password' />
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+        <Form.Check type='checkbox' label='Check me out' />
+      </Form.Group>
+      <Button variant='primary' type='submit' onClick={handleSubmitClick}>
+        Submit
+      </Button>
+    </Form>
+  );
+};
 export default CheckOutForm;
