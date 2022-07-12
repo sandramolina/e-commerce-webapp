f44a7117cf28b3cc2df9c2db0a4de30bf3b41ce5
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-12 19:46:41 +0100

    mc

--- src/components/homeCarousel/HomeCarousel.jsx
+++ src/components/homeCarousel/HomeCarousel.jsx
@@ -4,8 +4,6 @@ import { Carousel } from 'react-bootstrap';
 
 import './HomeCarousel.css';
 
-import './hc1.png';
-
 function HomeCarousel() {
   return (
     <Carousel id='carousel-container'>
@@ -20,14 +18,14 @@ function HomeCarousel() {
         <img
           className='d-block w-100'
           src='https://images.squarespace-cdn.com/content/v1/5b31a2cb96e76f3354ab0f15/1533762937393-75IK1GB5ENRJ2JKT0U1D/09-15-A1-FSS-in-your-element.jpg?format=2500w'
-          alt='Eyeshadows lipsticks sale'
+          alt='banner influencers showing Eyeshadow palettes'
         />
       </Carousel.Item>{' '}
       <Carousel.Item>
         <img
           className='d-block w-100'
           src='https://images.squarespace-cdn.com/content/v1/5b31a2cb96e76f3354ab0f15/1533762939640-LT78H14KSJBUKXO25C5H/09-20-A1-%2412-Palettes.jpg?format=2500w'
-          alt='Matte lipsticks sale'
+          alt='Eyeshadows banner'
         />
       </Carousel.Item>
     </Carousel>
