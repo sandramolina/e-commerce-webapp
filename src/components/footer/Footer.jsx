62d1f4ae80b94b0c2a49faa2112b15c46722e487
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-12 20:06:39 +0100

    adds footer translation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
 
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 import './Footer.css';
 
-const Footer = () => (
-  <>
-    <Container className='footer' fluid>
-      <Row>
-        <Col className='text-center container-top'>
-          <Button className='top' />
-        </Col>
-      </Row>
-      <Row className='text-center'>
-        <Col>
-          <h2>Follow SO Fetch!</h2>
-          <p>Social media icons</p>
-        </Col>
-        <hr />
-        <Col>
-          <h2>Inside SO Fetch!</h2>
-          <p>about SO Fetch!</p>
-          <p>Contact Us</p>
-          <p>Careers</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col className='text-center py-3'>
-          Made with 💜 by{' '}
-          <a href='https://github.com/sandramolina'>San Molina Palencia</a>
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Container className='footer' fluid>
+        <Row>
+          <Col className='text-center container-top'>
+            <Button className='top' />
+          </Col>
+        </Row>
+        <Row className='text-center'>
+          <Col>
+            <h2>{t('follow')} SO Fetch!</h2>
+            <p>Social media icons</p>
+          </Col>
+          <hr />
+          <Col>
+            <h2>{t('inside')} SO Fetch!</h2>
+            <p>{t('about_us')} SO Fetch!</p>
+            <p>{t('contact_us')}</p>
+            <p>{t('careers')}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='text-center py-3'>
+            Made with 💜 by{' '}
+            <a href='https://github.com/sandramolina'>San Molina Palencia</a>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default Footer;
