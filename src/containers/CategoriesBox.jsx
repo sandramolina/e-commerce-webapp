0856ff5585af7f42432c53d98a5a240ca62ce8c6
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-13 13:47:31 +0100

    chng url for categories

--- src/containers/CategoriesBox.jsx
+++ src/containers/CategoriesBox.jsx
@@ -14,28 +14,28 @@ function CategoriesBox() {
       title: 'EYES',
       string: t('eyes'),
       imageUrl:
-        'https://cdn.mos.cms.futurecdn.net/o2sQWDyqqjFDXhFfrQSqH6-1920-80.jpg.webp',
+        'https://raw.githubusercontent.com/sandramolina/assets/main/other/home-eyes.png',
     },
     {
       id: 2,
       title: 'FACE',
       string: t('face'),
       imageUrl:
-        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/02/22/20/istock-855759398.jpg?quality=75&width=990&auto=webp&crop=982:726,smart',
+        'https://raw.githubusercontent.com/sandramolina/assets/main/other/home-face.png',
     },
     {
       id: 3,
       title: 'LIPS',
       string: t('lips'),
       imageUrl:
-        'https://i0.wp.com/www.beautybucketeer.com/wp-content/uploads/2016/02/Rimmel-The-Only-1-Lipstick-Rectangle-1.jpg?resize=1080%2C810',
+        'https://raw.githubusercontent.com/sandramolina/assets/main/other/home-lips.png',
     },
     {
       id: 4,
       title: 'NAILS',
       string: t('nails'),
       imageUrl:
-        'https://glaminati.com/wp-content/uploads/2018/02/tp-shellac-nails-1.jpg',
+        'https://raw.githubusercontent.com/sandramolina/assets/main/other/home-nails.png',
     },
   ];
   const categoryNodes = categories.map((category) => (
